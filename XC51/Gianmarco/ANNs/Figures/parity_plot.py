fb13a27eccd6007b9ec3f0301ad1cd504c22db58
Emily <emwu@mit.edu>
2024-09-24 11:20:29 -0400

    Moved work for XC51 class to new repo

--- XC51/Gianmarco/ANNs/Figures/parity_plot.py
+++
@@ -1,2 +0,0 @@
-# This script generates parity plots for the random split ANN predictions (random seed of zero) across the seven datasets.
-# Ground truth is on the x-axis, and predictions are on the y-axis.
